6de3eb68451cb5af0b5a5a1c851aa184cff7a0b1
yu <yu@tk2-223-21469.vs.sakura.ne.jp>
2015-03-09 23:13:02 +0900

    correspond to sakura

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :photos
   root 'photos#index'
+  resources :photos
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
