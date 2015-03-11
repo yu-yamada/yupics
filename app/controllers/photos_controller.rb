c9a8284b978ee8ee84e9b2992c505e133a185f0a
yu <yu@tk2-223-21469.vs.sakura.ne.jp>
2015-03-11 23:13:32 +0900

    add basic auth

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,7 +4,8 @@ class PhotosController < ApplicationController
   # GET /photos
   # GET /photos.json
   def index
-    @photos = Photo.all
+    #@photos = Photo.all
+    @photos = Photo.order("date")
   end
 
   # GET /photos/1
